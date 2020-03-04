e6d538ae607d6dac8aff87e6d6606a90fa772478
MetalNeox <metalneox@gmail.com>
2020-03-05 00:55:21 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,23 @@
-function say(message: string){
-    return "msg: " + message
-}
+/*  JS
+var express = require('express');
+*/
 
-console.log(say("It Works!"))
+/*  TS
+import express from "express";
+*/
+
+import express from "express";
+
+const app = express();
+
+// define a route handler for the default home page
+app.get( "/", ( req:any, res:any ) => {
+    // render the index template
+  res.send('Hello World2!');
+} );
+
+// start the express server
+app.listen( 3000, () => {
+    // tslint:disable-next-line:no-console
+    console.log( 'server started at http://localhost:3000' );
+} );
